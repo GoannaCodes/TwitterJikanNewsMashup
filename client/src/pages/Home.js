bcd6317f65fd9c3204aa06526b452c5351712667
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-09-17 13:26:27 +1000

    Added home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home(){
+    return(
+        <div className="content">
+            <h1>Welcome to AniNews!</h1>
+            <p>Find the latest tweets and news about currently airing shows this season!</p>
+        </div>
+    )
+}
